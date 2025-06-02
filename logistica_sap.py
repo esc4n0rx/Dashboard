0f4a0db45e1db28fb5109a0ca97912472f7ef7f7
esc4n0rx <paulo.cunha@hortifruti.com.br>
2025-06-02 11:11:05 -0300

    feat: Adicionar suporte para datas dinâmicas na extração de dados do SAP

--- logistica_sap.py
+++ logistica_sap.py
@@ -6,18 +6,22 @@ import time
 import pythoncom
 import win32com.client
 import streamlit as st
+from datetime import datetime, timedelta
 from logistica_logger import log_info, log_erro, log_sucesso, log_debug
 
 def extrair_dados_sap():
-    """
-    Extrai dados do SAP usando automação com pywin32.
-    
-    Returns:
-        bool: True se a extração for bem-sucedida, False caso contrário.
-    """
     log_info("Iniciando extração de dados do SAP...", mostrar_ui=False)
     
     try:
+        hoje = datetime.now()
+        ontem = hoje - timedelta(days=1)
+        
+        data_alta = hoje.strftime("%d.%m.%Y")
+        data_baixa = ontem.strftime("%d.%m.%Y")
+        
+        log_debug(f"Data de início da extração: {data_baixa}")
+        log_debug(f"Data de fim da extração: {data_alta}")
+
         # Inicializa COM para thread atual
         log_debug("Inicializando COM...")
         pythoncom.CoInitialize()
@@ -46,6 +50,11 @@ def extrair_dados_sap():
         session.findById("wnd[1]/usr/cntlALV_CONTAINER_1/shellcont/shell").currentCellRow = 1
         session.findById("wnd[1]/usr/cntlALV_CONTAINER_1/shellcont/shell").selectedRows = "1"
         session.findById("wnd[1]/usr/cntlALV_CONTAINER_1/shellcont/shell").doubleClickCurrentCell
+        
+        # Inserção das datas dinâmicas
+        session.findById("wnd[0]/usr/ctxtSO_PDATU-LOW").text = data_baixa
+        session.findById("wnd[0]/usr/ctxtSO_PDATU-HIGH").text = data_alta
+        
         session.findById("wnd[0]").sendVKey(0)
         session.findById("wnd[0]/tbar[1]/btn[8]").press()
         log_debug("Iniciada extração de dados...")
